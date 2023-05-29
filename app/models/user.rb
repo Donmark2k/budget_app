7ba2798dddc58b9423a4291bd4d418a396a39495
Donmark2k <chukwuemekavp@gmail.com>
2023-05-29 15:56:34 +0100

    Add the association in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    has_many :expenses, dependent: :destroy
+    has_many :groups, dependent: :destroy
+
 
 
     validates :name, presence: true, length: { minimum: 2, maximum: 50 }
