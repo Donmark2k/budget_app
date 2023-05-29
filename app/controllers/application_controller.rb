ac9c7d3670bf6d7ae16c42f0011aee0b08bbb777
Donmark2k <chukwuemekavp@gmail.com>
2023-05-30 00:29:48 +0100

    Run migration to role to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  # protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to request.referrer, alert: exception.message
+  end
 
   protected
 
