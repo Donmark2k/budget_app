f88190f71b58897850d1f25fcc936d40db131e6a
Donmark2k <chukwuemekavp@gmail.com>
2023-05-29 15:41:33 +0100

    Add authorid column to expenses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_141840) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_142224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "author_id"
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -29,5 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_141840) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "expenses", "users", column: "author_id"
   add_foreign_key "groups", "users"
 end
