477b3790c2195b7c6ae6061f3af0f447bf7e1fcd
Donmark2k <chukwuemekavp@gmail.com>
2023-05-29 15:53:17 +0100

    Validate name in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+
+    validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_142224) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_144418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_142224) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
+  end
+
+  create_table "expenses_groups", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "expenses_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expenses_id"], name: "index_expenses_groups_on_expenses_id"
+    t.index ["group_id"], name: "index_expenses_groups_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -38,5 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_142224) do
   end
 
   add_foreign_key "expenses", "users", column: "author_id"
+  add_foreign_key "expenses_groups", "expenses", column: "expenses_id"
+  add_foreign_key "expenses_groups", "groups"
   add_foreign_key "groups", "users"
 end
