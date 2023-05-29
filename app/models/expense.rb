4524577c95837ae41fcc740f6109c7ab5cf96ab4
Donmark2k <chukwuemekavp@gmail.com>
2023-05-29 16:11:09 +0100

    Add the association in the expense_group model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,6 @@ class Expense < ApplicationRecord
     belongs_to :author
   has_many :expenses_groups, dependent: :destroy
   has_many :groups, through: :expenses_groups
+  validates :name, presence: true
+  validates :amount, numericality: { is_decimal: true }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :expenses_groups, dependent: :destroy
   has_many :expenses, through: :expenses_groups
+  validates :name, :icon, presence: true
+
 
 end
