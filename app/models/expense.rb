0560ff22bddf863ad70a3047c3c9be4b47045f9d
Donmark2k <chukwuemekavp@gmail.com>
2023-05-29 16:04:49 +0100

    Add the association in the expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,5 @@
 class Expense < ApplicationRecord
+    belongs_to :author
+  has_many :expenses_groups, dependent: :destroy
+  has_many :groups, through: :expenses_groups
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many :groups, dependent: :destroy
+  has_many :expenses_groups, dependent: :destroy
+  has_many :expenses, through: :expenses_groups
+
 end
